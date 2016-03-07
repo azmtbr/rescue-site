43f089597ade05625a30041472958f87aeec500f
Jon Malis <jmalis@live.com>
2016-03-06 21:06:29 -0700

    trying to fix routes

--- app/scripts/controllers/listingController.js
+++ app/scripts/controllers/listingController.js
@@ -1,12 +1,19 @@
 (function() {
-  function listingController($scope, Restangular) {
+  function listingController($scope, Restangular, $stateParams) {
 
-    $scope.animals = Restangular.all('rescues/1/animals').getList().$object;
+    // $scope.animals = Restangular.one('rescues/', $stateParams['rescue_id']).getList('animals').$object;
 
+    Restangular.one('rescues').getList().then(function(rescues) {
+      $scope.rescue = rescues[0];
+    })
+
+
+    $scope.animals = $scope.rescue.getList('animals').$object;
+    debugger
   }
 
 
   angular
 		.module('rescueSite')
-		.controller('listingController', ['$scope', 'Restangular', listingController]);
+		.controller('listingController', ['$scope', 'Restangular', '$stateParams', listingController]);
 })();
