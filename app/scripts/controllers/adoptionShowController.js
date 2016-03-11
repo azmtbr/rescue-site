990dd92f1d6eb45412423eb9edde6e300d00976d
Jon Malis <jmalis@live.com>
2016-03-10 20:20:34 -0700

    adoption form notice sending and print functionality in progress

--- app/scripts/controllers/adoptionShowController.js
+++ app/scripts/controllers/adoptionShowController.js
@@ -1,20 +1,20 @@
 (function() {
   function adoptionShowController($scope, Restangular, $stateParams) {
 
-    // Restangular.one('adoptions', $stateParams['id']).get().then(function(adoption) {
-    //   $scope.adoption = adoption;
-    // })
-    // $scope.selectedForm = false;
-    //
-    // $scope.openForm = function() {
-    //   $scope.selectedForm = true;
-    // }
 
     $scope.isCollapsed = true;
-
     $scope.adoptions = Restangular.all('adoptions').getList().$object;
-
     $scope.adoption = Restangular.one('adoptions', 2).get().$object;
+
+    $scope.printTo = function (printSection) {
+
+      var innerContents = document.getElementById('printSection').innerHTML;
+      var popupWinindow = window.open('', '_blank', 'width=600,height=700,scrollbars=no,menubar=no,toolbar=no,location=no,status=no,titlebar=no');
+      popupWinindow.document.open();
+      popupWinindow.document.write('<html><head><link rel="stylesheet" type="text/css" href="style.css" /></head><body onload="window.print()">' + innerContents + '</html>');
+      popupWinindow.document.close();
+    }
+
 
   }
 
--- dist/scripts/controllers/adoptionFormController.js
+++ dist/scripts/controllers/adoptionFormController.js
@@ -5,8 +5,12 @@
                        city: "", state: "", postal_code: "", home_phone: "", cell_phone: "",
                        work_phone: "", desired_animal: ""};
 
+    $scope.adoptionNotice = {message: "You have a new adoption form to review. Click the link below to view the form in the admin section of your website",
+                             link: "localhost:3000/adoption-forms"};
+
 
     $scope.sendAdoptionForm = function(contact) {
+      Restangular.one('').post('adoptionnotices', $scope.adoptionNotice);
       Restangular.one('').post('adoptions', $scope.adoption);
       $scope.adoption = {name: "", email: "", age: false, street_address: "", mailing_address: "",
                          city: "", state: "", postal_code: "", home_phone: "", cell_phone: "",
