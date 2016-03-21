37987fd94d5811b7175ce55d3c26e5e776e4a4f2
Jon Malis <jmalis@live.com>
2016-03-20 18:05:15 -0700

    gallery and photo linked, still working on showing in profile view

--- dist/scripts/controllers/profileController.js
+++ dist/scripts/controllers/profileController.js
@@ -2,14 +2,21 @@
   function profileController($scope, $location, Restangular, FileUploader, $stateParams) {
 
     Restangular.one('animals', $stateParams['slug']).get().then(function(animal) {
-      $scope.uploader = new FileUploader({url: "http://127.0.0.1:4000/api/rescues/1/animals/" + animal.slug,
-                                          method: 'PATCH'});
+      // $scope.uploader = new FileUploader({url: "http://127.0.0.1:4000/api/rescues/1/animals/" + animal.slug,
+      //                                     method: 'PATCH'});
       $scope.animal = animal;
     })
 
     $scope.isAdmin = true;
 
-    $scope.gallery = Restangular.one('galleries', 1).get().$object;
+    Restangular.one('galleries', 1).get().then(function(gallery) {
+      return $scope.gallery = gallery;
+    })
+
+    Restangular.one('photos', 1).get().then(function(photos) {
+      $scope.uploader = new FileUploader({url: "http://127.0.0.1:4000/api/rescues/1/animals/" + photos.gallery_id,
+                                          method: 'PATCH'});
+    })
 
 
 
