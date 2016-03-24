0bdced3c8fa140160f7e5e4b42e9f49b416ed923
Jon Malis <jmalis@live.com>
2016-03-24 15:17:44 -0700

    both uploaders now work on profile page

--- app/scripts/controllers/profileController.js
+++ app/scripts/controllers/profileController.js
@@ -2,13 +2,13 @@
   function profileController($scope, $location, Restangular, FileUploader, $stateParams) {
 
     Restangular.one('animals', $stateParams['slug']).get().then(function(animal) {
-      // $scope.uploader = new FileUploader({url: "http://127.0.0.1:4000/api/rescues/$RESCUE_ID/animals/" + animal.slug,
-      //                                     method: 'PATCH'});
+      $scope.profileUploader = new FileUploader({url: "http://127.0.0.1:4000/api/rescues/$RESCUE_ID/animals/" + animal.slug,
+                                          method: 'PATCH'});
       $scope.animal = animal;
 
       Restangular.one('galleries', animal.gallery_id).get().then(function(gallery) {
         $scope.gallery = gallery;
-        $scope.uploader = new FileUploader({url: "http://127.0.0.1:4000/api/rescues/$RESCUE_ID/galleries/" + animal.gallery_id + "/photos",
+        $scope.galleryUploader = new FileUploader({url: "http://127.0.0.1:4000/api/rescues/$RESCUE_ID/galleries/" + animal.gallery_id + "/photos",
                                             method: 'POST',
                                             alias: "photo[gallery_image]"});
       })
