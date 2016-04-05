c12ddd8f0bb4295c0cd0451cd3a0ba2c8c4186ae
Matthieu Tanguay-Carel <matthieutc@gmail.com>
2016-04-05 18:14:48 +0200

    updates deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git checkout deploy && git pull --rebase origin master && grunt build && git push heroku deploy:master && git checkout master
+git checkout deploy && git pull --rebase origin master && grunt build && git commit -am "automated build" && git push --force heroku deploy:master && git checkout master
