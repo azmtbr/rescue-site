9c9cecc1fcbee4fd88e862b9fec8a18b4b5fc431
Jon Malis <jmalis@live.com>
2016-04-15 13:17:56 -0700

    landing manual slider instead of carousel

--- app/scripts/controllers/landingController.js
+++ app/scripts/controllers/landingController.js
@@ -21,7 +21,7 @@
           for (var i = files.length - 1; i >= 0; i--)
 
             Upload.upload({
-                url: "https://rescue-site-api.herokuapp.com/api/rescues/" + slug + "/landing_galleries/" + rescue.landing_gallery.id + "/landing_images",
+                url: "http://127.0.0.1:4000/api/rescues/" + slug + "/landing_galleries/" + rescue.landing_gallery.id + "/landing_images",
                 headers: $auth.retrieveData('auth_headers'),
                 method: 'POST',
                 data: {"landing_image[landing_image]": files[i]}
