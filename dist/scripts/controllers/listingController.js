9d74538f62f8e10ba821b064365d4213f311cbfb
Jon Malis <jmalis@live.com>
2016-05-14 15:11:22 -0700

    add and remove animal working. Needs styling

--- dist/scripts/controllers/listingController.js
+++ dist/scripts/controllers/listingController.js
@@ -1,13 +1,42 @@
 (function() {
-  function listingController($scope, Restangular, $stateParams) {
+  function listingController($scope, $timeout, Restangular, $stateParams) {
 
     Restangular.one('rescues', slug).all('animals').getList().then(function(animals) {
       $scope.animals = animals;
     })
-  }
 
+
+
+    Restangular.one('rescues', slug).get().then(function(rescue) {
+      $scope.rescue = rescue
+
+      Restangular.one('rescues', slug).one('animals', $stateParams['slug']).get().then(function(animal) {
+        $scope.animal = {rescue_id: rescue.id};
+      })
+
+
+      $scope.addAnimal = function() {
+        Restangular.one('').post('rescues/' + rescue.id + '/animals', $scope.animal);
+        $scope.animalCreated = true;
+        Restangular.one('rescues', slug).all('animals').getList().then(function(animals) {
+          $scope.animals = animals;
+        })
+        $timeout(function () {
+          $scope.animalCreated = false;
+        }, 5000);
+      }
+
+      $scope.deleteAnimal = function(animal) {
+        Restangular.one('rescues', slug).one('animals', animal.id).remove().then(function(){
+          $scope.animals = _.without($scope.animals, animal);
+        });
+      }
+    })
+
+
+  }
 
   angular
 		.module('rescueSite')
-		.controller('listingController', ['$scope', 'Restangular', '$stateParams', listingController]);
+		.controller('listingController', ['$scope', '$timeout', 'Restangular', '$stateParams', listingController]);
 })();
